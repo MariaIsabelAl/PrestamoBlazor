79b4cc2178f25154ed6cbecf24381f87eb495ba3
Maria Isabel Alberto Del Orbe <maria_alberto1@ucne.edu.do>
2020-11-05 15:17:19 -0400

    Add project files.

---
+++ Controllers/PersonasController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PrestamoBlazor.BLL;
+using PrestamoBlazor.Models;
+
+namespace ApiPrestamo.Controllers
+{
+    [Route("apiPrestamos/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Personas>> Get()
+        {
+            return PersonasBLL.GetPersona();
+        }
+
+        // GET api/<PersonaController>/5
+        [HttpGet("{id}")]
+        public ActionResult<List<Personas>> Get(int id)
+        {
+            return PersonasBLL.GetPersona();
+        }
+
+        // POST api/<PersonaController>
+        [HttpPost]
+        public void Post([FromBody] Personas persona)
+        {
+            PersonasBLL.Guardar(persona);
+        }
+
+
+        // DELETE api/<PersonaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            PersonasBLL.Eliminar(id);
+        }
+    }
+}
---
+++ Controllers/PrestamosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PrestamoBlazor.BLL;
+using PrestamoBlazor.DAL;
+using PrestamoBlazor.Models;
+
+namespace ApiPrestamo.Controllers
+{
+    [Route("apiPrestamos/[controller]")]
+    [ApiController]
+    public class PrestamosController : ControllerBase
+    {
+        // GET: api/<PrestamosController>
+        [HttpGet]
+        public ActionResult<List<Prestamos>> Get()
+        {
+            return PrestamoBLL.GetPrestamos();
+        }
+
+        // GET api/<PrestamosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PrestamosController>
+        [HttpPost]
+        public void Post([FromBody] Prestamos prestamos)
+        {
+            PrestamoBLL.Guardar(prestamos);
+        }
+
+
+
+        // DELETE api/<PrestamosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            PrestamoBLL.Eliminar(id);
+        }
+    }
+}
